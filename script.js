15ee4d8438d36d79d47dba96604ae9333d751e90
Munna <enyisuccesskel@gmail.com>
2022-06-22 22:44:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var yourName, height, country;
+yourName = prompt("Enter your Name");
+height = prompt("State your Height");
+country = prompt("State your Country");
+
+document.write(`${yourName}
+${height}
+${country}`);
